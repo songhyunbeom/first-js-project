52f73a06450d422d2434fec9ff03b028595cfc01
songhyunbeom <tiger8359@gmail.com>
2022-01-05 23:36:04 +0900

    second commit

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.png", "2.jpg", "3.jpg"]
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"]
 
 const chowenImage = images[Math.floor(Math.random() * images.length)]
 
--- js/todo.js
+++ js/todo.js
@@ -25,7 +25,7 @@ function paintToDo(newTodo){
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "✊"
+    button.innerText = "❌"
     button.addEventListener("click",deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
